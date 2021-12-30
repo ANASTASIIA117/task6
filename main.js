f8ba2c3181285a73b198cd8a5241399800c9c4e6
ANASTASIIA117 <89926663+ANASTASIIA117@users.noreply.github.com>
2021-12-30 16:17:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+btn.addEventListener("click",fnc);
+function fnc(){
+    console.log(a.checked);
+    console.log(б.checked);
+    console.log(в.checked);
+    console.log(г.checked);
+    
+    if (б.checked){
+        alert("Правильно");
+    } else {
+        alert("Не верно");
+    }
+}
